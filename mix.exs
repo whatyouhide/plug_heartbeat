d5e15e8a7619cc370fcefa990e394489dae09029
Andrea Leopardi <an.leopardi@gmail.com>
2015-09-27 16:28:26 +0200

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PlugHeartbeat.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @github_url "https://github.com/whatyouhide/plug_heartbeat"
   @description """
   A tiny plug for responding to heartbeat requests
